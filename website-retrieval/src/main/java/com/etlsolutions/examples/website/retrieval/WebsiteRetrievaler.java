bcc26a97ebd11fda43dbb2239572b6264dee6e28
gcodegroup <gcodegroup@gmail.com>
2018-03-22 14:55:50 +0000

    web crawler

--- website-retrieval/src/main/java/com/etlsolutions/examples/website/retrieval/WebsiteRetrievaler.java
+++ website-retrieval/src/main/java/com/etlsolutions/examples/website/retrieval/WebsiteRetrievaler.java
@@ -1,10 +1,13 @@
 package com.etlsolutions.examples.website.retrieval;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.HashSet;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 /**
@@ -16,6 +19,8 @@ public final class WebsiteRetrievaler {
     private static final int MAX_DEPTH = 2;
     private final HashSet<String> links = new HashSet<>();
 
+    private static int fileIndex = 1;
+    
     public void getPageLinks(String URL, int depth) {
         
         if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
@@ -24,6 +29,12 @@ public final class WebsiteRetrievaler {
                 links.add(URL);
 
                 Document document = Jsoup.connect(URL).get();
+                List<TextNode> textMpdes = document.textNodes();
+                
+                for(TextNode node : textMpdes){
+                    FileUtils.writeStringToFile(new File ("/Temp/craeler/" + (100000 + fileIndex++) + ".txt"), node.text(), "UTF-8");
+                }
+                
                 Elements linksOnPage = document.select("a[href]");
 
                 depth++;
