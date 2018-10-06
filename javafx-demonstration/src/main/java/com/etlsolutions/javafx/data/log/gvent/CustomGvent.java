ec733e9dac33c9dbb42ed2648290338cdced126c
eep60b <eep60b@gmail.com>
2018-10-06 22:17:20 +0100

    add area

--- javafx-demonstration/src/main/java/com/etlsolutions/javafx/data/log/gvent/CustomGvent.java
+++ javafx-demonstration/src/main/java/com/etlsolutions/javafx/data/log/gvent/CustomGvent.java
@@ -1,9 +1,5 @@
 package com.etlsolutions.javafx.data.log.gvent;
 
-import com.etlsolutions.javafx.data.log.Log;
-import com.etlsolutions.javafx.data.log.Notification;
-import java.util.List;
-
 /**
  *
  * @author zc
