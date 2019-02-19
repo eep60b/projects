c72414bee2c47139f4ad04b036162fc2f82d62dc
gcodegroup <gcodegroup@gmail.com>
2019-02-19 10:02:37 +0000

    Palette

---
+++ javafx-demonstration/src/main/java/com/etlsolutions/javafx/presentation/palette/PaletteController.java
@@ -0,0 +1,16 @@
+package com.etlsolutions.javafx.presentation.palette;
+
+import com.etlsolutions.javafx.AbstractFXMLController;
+
+/**
+ * FXML Controller class
+ *
+ * @author zc
+ */
+public class PaletteController extends AbstractFXMLController {
+
+    @Override
+    public void initializeComponents() {
+        
+    }
+}
